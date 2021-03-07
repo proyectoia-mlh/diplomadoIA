380654fc220bb5fc4c44640be5f344ae6f2aaff5
Miller Leon <mjleonh@hotmail.com>
2021-03-06 23:29:55 -0500

    Create Intro_Python.py
    version inicial

---
+++ Intro_Python.py
@@ -0,0 +1,26 @@
+print("Hello friend. Hello friend?")
+print("hola\nmundo")
+# usando print para imprimir un texto
+print("Sólo sé...\n...que nada sé.")
+
+# Asignamos 1 a la variable x
+x=1
+print("El valor de la variable x es ",x)
+# Asignamos "I'm ready to code!" a la variable y
+y="I'm ready to code!"
+print("El valor de la variable y es",y)
+
+x = 5.6
+print('x es número real:', x)
+x = 3+4j
+print('x ahora es un número complejo:',x)
+x = '25 de mayo'
+print('y ahora x es un string:', x)
+
+# Tipos de datos
+x = 1
+print(x)
+print(type(x))
+x = 1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001
+print(x)
+print(type(x))
